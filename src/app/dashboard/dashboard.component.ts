6332c00430e8b5903395dc304c30a278612f43e5
ThamaliiNirmala <tnirmalaadasooriya@gmail.com>
2022-04-06 06:16:05 +0530

    All CRUD part done

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,6 +12,9 @@ export class DashboardComponent implements OnInit {
 
   formValue !: FormGroup;
   studentModelObj : StudentModel = new StudentModel();
+  studentData !: any;
+  showAdd !: boolean;
+  showUpdate !: boolean
   constructor(private formbuilder: FormBuilder,
     private api : ApiService) { }
 
@@ -23,6 +26,12 @@ export class DashboardComponent implements OnInit {
       contact : [''],
       gpa : ['']
     })
+    this.getAllStudent();
+  }
+  clickAddStudent(){
+    this.formValue.reset();
+    this.showAdd = true;
+    this.showUpdate = false;
   }
   postStudentDetails(){
       this.studentModelObj.firstName = this.formValue.value.firstName;
@@ -38,9 +47,49 @@ export class DashboardComponent implements OnInit {
         let ref = document.getElementById('close')
         ref?.click();
         this.formValue.reset();
+        this.getAllStudent();
       },
       err=>{
         alert("Something went Wrong")
+      })
+  }
+  getAllStudent(){
+    this.api.getStudent()
+    .subscribe(res=>{
+      this.studentData = res;
+    })
+  }
+  deleteStudent(row : any){
+    this.api.deleteStudent(row.id)
+    .subscribe(res=>{
+      alert("Successfully Student Record Deleted");
+      this.getAllStudent();
+    })
+  }
+  onEdit(row: any){
+    this.showAdd = false;
+    this.showUpdate = true;
+    this.studentModelObj.id = row.id;
+    this.formValue.controls['firstName'].setValue(row.firstName)
+    this.formValue.controls['lastName'].setValue(row.lastName)
+    this.formValue.controls['email'].setValue(row.email)
+    this.formValue.controls['contact'].setValue(row.contact)
+    this.formValue.controls['gpa'].setValue(row.gpa)
+  }
+  updateStudentDetails(){
+    this.studentModelObj.firstName = this.formValue.value.firstName;
+      this.studentModelObj.lastName = this.formValue.value.lastName;
+      this.studentModelObj.email = this.formValue.value.email;
+      this.studentModelObj.contact = this.formValue.value.contact;
+      this.studentModelObj.gpa = this.formValue.value.gpa;
+
+      this.api.updateStudent(this.studentModelObj, this.studentModelObj.id)
+      .subscribe(res=>{
+        alert("Student Details Updated Successfully");
+        let ref = document.getElementById('close')
+        ref?.click();
+        this.formValue.reset();
+        this.getAllStudent();
       })
   }
 
